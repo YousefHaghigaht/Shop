048c6f8d0e0eb73c1e2fb9c310df31a5caf931be
yousef <yousefmpym1@gmail.com>
2025-02-17 13:23:23 +0330

    Dockerized project

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('register/',views.UserRegistrationView.as_view(),name='register'),
+    path('verify_code/',views.UserVerifyCodeView.as_view(),name='verify_code'),
+    path('login/',views.UserLoginView.as_view(),name='login'),
+    path('logout/',views.UserLogoutView.as_view(),name='logout'),
+    path('resend_code/',views.ResendCodeView.as_view(),name='resend_code')
+]
