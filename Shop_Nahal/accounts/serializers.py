5362e1afeac30502441c50b8a765b97ef6b1885d
YousefHaghighat <yousefmpym1@gmail.com>
2025-02-17 13:55:19 +0330

    Delete Shop_Nahal directory

--- Shop_Nahal/accounts/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-
-class UsersSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('phone_number','email','full_name','password')
-
--- Shop_Nahal/orders/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Order,Coupon
-
-admin.site.register(Order)
-admin.site.register(Coupon)
